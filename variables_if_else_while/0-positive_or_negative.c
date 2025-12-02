56545d714704ea38ac3495c24c1814fc82066cb1
Shola <sholagulmaliyeva@gmail.com>
2025-12-02 20:32:02 +0000

    Add 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,14 @@
-nano 0-positive_or_negative.c
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
