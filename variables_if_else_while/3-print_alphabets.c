b2a48521b2f21180155027ee1bf3a7fad9a75626
Shola <sholagulmaliyeva@gmail.com>
2025-12-02 21:23:05 +0000

    Adding 3-print_alphabets.c to variables_if_else_while

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, followed by a new line
+ * main - prints the alphabet in lowercase and uppercase
+ *        followed by a new line
  *
  * Return: Always 0
  */
@@ -11,6 +12,10 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
 	putchar('\n');
 
 	return (0);
