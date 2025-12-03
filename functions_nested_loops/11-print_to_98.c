0f9f510a4f95ce3fdbce05647b47f1e2e22ee311
Shola <sholagulmaliyeva@gmail.com>
2025-12-03 20:47:17 +0000

    editing 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,29 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
- *
- * Return: void
+ * print_number - prints an integer using _putchar
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n <= -10)
+			print_number(-(n / 10));
+		_putchar('0' - (n % 10));
+	}
+	else
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: starting number
  */
 void print_to_98(int n)
 {
@@ -13,29 +32,4 @@ void print_to_98(int n)
 	if (n <= 98)
 		step = 1;
 	else
-		step = -1;
-
-	while (n != 98)
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			if (-n >= 10)
-				_putchar((-n / 10) + '0');
-			_putchar((-n % 10) + '0');
-		}
-		else
-		{
-			if (n >= 10)
-				_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		_putchar(',');
-		_putchar(' ');
-		n += step;
-	}
-
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
-}
+		step =
