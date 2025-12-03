65e34d8ff17c05f2b1755156edf62f4fbc9f55ca
Shola <sholagulmaliyeva@gmail.com>
2025-12-03 20:40:03 +0000

    editing 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,25 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
+			if (i < 0)
 			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				_putchar('-');
+				if (-i >= 10)
+					_putchar((-i / 10) + '0');
+				_putchar((-i % 10) + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
 			}
 			else
+				_putchar(i + '0');
+
+			if (i != 98)
 			{
-				_putchar('9');
-				_putchar('8');
-				_putchar('\n');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
@@ -33,31 +40,28 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
+			if (i < 0)
+			{
+				_putchar('-');
+				if (-i >= 10)
+					_putchar((-i / 10) + '0');
+				_putchar((-i % 10) + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+				_putchar(i + '0');
+
 			if (i != 98)
 			{
-				if (i >= 100 || i <= -100)
-				{
-					_putchar((i / 100) % 10 + '0');
-					_putchar((i / 10) % 10 + '0');
-					_putchar(i % 10 + '0');
-				}
-				else if (i >= 10 || i <= -10)
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-				}
-				else
-					_putchar(i + '0');
-
 				_putchar(',');
 				_putchar(' ');
 			}
-			else
-			{
-				_putchar('9');
-				_putchar('8');
-				_putchar('\n');
-			}
 		}
 	}
+	_putchar('\n');
 }
+
