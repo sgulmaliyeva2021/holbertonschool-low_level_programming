2a9c9e80e8042d1f95965c146c01987121388723
Shola <sholagulmaliyeva@gmail.com>
2025-12-03 20:44:09 +0000

    editing 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,56 +1,70 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using _putchar
- * @n: the number to print
- */
-void print_number(int n)
-{
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10)
-        print_number(n / 10);
-
-    _putchar((n % 10) + '0');
-}
-
-/**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
+ * @n: the starting number
  *
  * Return: void
  */
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            print_number(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            print_number(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				if (i < 10 && i >= 0)
+					_putchar(i + '0');
+				else if (i < 0)
+				{
+					_putchar('-');
+					_putchar(-i / 10 + '0');
+					_putchar(-i % 10 + '0');
+				}
+				else
+				{
+					_putchar(i / 10 + '0');
+					_putchar(i % 10 + '0');
+				}
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				if (i >= 0)
+				{
+					_putchar(i / 10 + '0');
+					_putchar(i % 10 + '0');
+				}
+				else
+				{
+					_putchar('-');
+					_putchar(-i / 10 + '0');
+					_putchar(-i % 10 + '0');
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('9');
+				_putchar('8');
+			}
+		}
+	}
+	_putchar('\n');
 }
