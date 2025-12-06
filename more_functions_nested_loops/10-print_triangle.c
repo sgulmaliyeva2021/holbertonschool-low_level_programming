f8f385fc0b724cedcd577773f5dc993414612947
root <sgulmaliyeva2021@gmail.com>
2025-12-06 19:17:44 +0000

    adding 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		/* print leading spaces */
+		for (j = 1; j <= size - i; j++)
+			_putchar(' ');
+
+		/* print hashes */
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
