e3d79c878c99c62c86ca3d67ed6e47e57793f7ee
root <sgulmaliyeva2021@gmail.com>
2025-12-06 19:09:15 +0000

    editin style

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
  */
 void print_numbers(void)
 {
-    char c;
+	int c;
 
-    for (c = '0'; c <= '9'; c++)
-        _putchar(c);
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
 
-    _putchar('\n');
+	_putchar('\n');
 }
