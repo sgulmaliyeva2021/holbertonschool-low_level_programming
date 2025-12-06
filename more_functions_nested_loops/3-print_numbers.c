3ae70f66b16ca820c94a0d5ae2b91a6f17c0299e
root <sgulmaliyeva2021@gmail.com>
2025-12-06 19:07:48 +0000

    adding new c files

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,26 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_numbers - prints the numbers from 0 to 9
- *                  followed by a new line
+ * print_numbers - prints the numbers 0 to 9
+ *
+ * Return: void
  */
 void print_numbers(void)
 {
-	char c;
+    char c;
 
-	for (c = '0'; c <= '9'; c++)
-		putchar(c);
+    for (c = '0'; c <= '9'; c++)
+        _putchar(c);
 
-	putchar('\n');
-}
-
-/**
- * main - test print_numbers
- *
- * Return: Always 0
- */
-int main(void)
-{
-	print_numbers();
-	return (0);
+    _putchar('\n');
 }
