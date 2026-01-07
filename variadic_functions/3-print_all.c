2d3354ba7847ca36f084286cf22d39ce6075acaf
SHola Gulmaliyeva <sholagulmaliyeva@gmail.com>
2026-01-07 10:24:47 +0000

    adding new

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,10 +4,11 @@
 
 /**
  * print_all - prints anything based on a format string
- * @format: list of types of arguments passed to the function
+ * @format: list of types (c = char, i = int, f = float, s = string)
  *
- * Description: c = char, i = integer, f = float, s = string
- * Any other char is ignored. Prints (nil) for NULL strings.
+ * Prints a comma and space between arguments.
+ * Prints (nil) if a string is NULL.
+ * Ignores unknown format characters.
  */
 void print_all(const char * const format, ...)
 {
@@ -18,24 +19,33 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format != NULL && format[i] != '\0')
+	/* First top-level if: only enter if format is valid */
+	if (format != NULL)
 	{
-		if (format[i] == 'c')
-			printf("%s%c", sep, va_arg(args, int));
-		if (format[i] == 'i')
-			printf("%s%d", sep, va_arg(args, int));
-		if (format[i] == 'f')
-			printf("%s%f", sep, va_arg(args, double));
-		if (format[i] == 's')
+		while (format[i] != '\0')
 		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("%s(nil)", sep);
-			else
-				printf("%s%s", sep, str);
+			/* Second top-level if: handle allowed format characters */
+			if (format[i] == 'c' || format[i] == 'i' ||
+			    format[i] == 'f' || format[i] == 's')
+			{
+				if (format[i] == 'c')
+					printf("%s%c", sep, va_arg(args, int));
+				if (format[i] == 'i')
+					printf("%s%d", sep, va_arg(args, int));
+				if (format[i] == 'f')
+					printf("%s%f", sep, va_arg(args, double));
+				if (format[i] == 's')
+				{
+					str = va_arg(args, char *);
+					if (str == NULL)
+						printf("%s(nil)", sep);
+					else
+						printf("%s%s", sep, str);
+				}
+				sep = ", ";
+			}
+			i++;
 		}
-		sep = ", ";
-		i++;
 	}
 
 	va_end(args);
