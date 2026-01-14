0a43f2ca247e805a4b4d4f2fbc88f12ff4552f5d
Shola <sholagulmaliyeva@gmail.com>
2026-01-14 10:19:23 +0000

    adding all over again

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,25 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string
- * @len: length of the string
- * @next: points to the next node
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
  */
-typedef struct list_s
+size_t print_list(const list_t *h)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	size_t count = 0;
 
-/* Function prototypes */
-size_t print_list(const list_t *h);
-int _putchar(char c);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-#endif /* LISTS_H */
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
