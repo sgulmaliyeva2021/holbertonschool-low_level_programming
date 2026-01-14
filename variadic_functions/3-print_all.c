cf51e381ac77609a291e810e7f5398afa3accc2e
Shola <sholagulmaliyeva@gmail.com>
2026-01-14 11:22:58 +0000

    editing 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,43 +1,40 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * print_all - prints anything based on a format string
- * @format: list of types of arguments passed
+ * @format: list of types (c: char, i: int, f: float, s: string)
  *
- * Description: c = char, i = int, f = float, s = string
- * Ignored any other characters. Prints (nil) if string is NULL.
+ * Allowed: max 2 ifs, no else if
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	int printed = 0;
+    va_list args;
+    unsigned int i = 0;
+    int printed = 0;
+    char *str;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i])
-	{
-		if (printed)
-			printf(", ");
-		if (format[i] == 'c')
-			printf("%c", va_arg(args, int)), printed = 1;
-		if (format[i] == 'i')
-			printf("%d", va_arg(args, int)), printed = 1;
-		if (format[i] == 'f')
-			printf("%f", va_arg(args, double)), printed = 1;
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s", str), printed = 1;
-		}
-		i++;
-	}
+    while (format && format[i])
+    {
+        if (printed)
+            printf(", ");
 
-	va_end(args);
-	printf("\n");
+        /* Single if for all types */
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+        {
+            printed = 1;
+
+            format[i] == 'c' && printf("%c", va_arg(args, int));
+            format[i] == 'i' && printf("%d", va_arg(args, int));
+            format[i] == 'f' && printf("%f", va_arg(args, double));
+            format[i] == 's' && ((str = va_arg(args, char *)) ? printf("%s", str) : printf("(nil)"));
+        }
+        i++;
+    }
+
+    va_end(args);
+    printf("\n");
 }
