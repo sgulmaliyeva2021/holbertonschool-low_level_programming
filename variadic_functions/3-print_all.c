367918e14853ce7b9f2c324550d95745da8a82e2
Shola <sholagulmaliyeva@gmail.com>
2026-01-14 11:19:56 +0000

    editing 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,51 +1,41 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_all - prints anything based on a format string
- * @format: list of types (c = char, i = int, f = float, s = string)
+ * @format: list of types of arguments passed
  *
- * Prints a comma and space between arguments.
- * Prints (nil) if a string is NULL.
- * Ignores unknown format characters.
+ * Description: c = char, i = int, f = float, s = string
+ * Ignored any other characters. Prints (nil) if string is NULL.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
 	char *str;
-	char *sep = "";
+	int printed = 0;
 
 	va_start(args, format);
 
-	/* First top-level if: only enter if format is valid */
-	if (format != NULL)
+	while (format && format[i])
 	{
-		while (format[i] != '\0')
+		if (printed)
+			printf(", ");
+		if (format[i] == 'c')
+			printf("%c", va_arg(args, int)), printed = 1;
+		if (format[i] == 'i')
+			printf("%d", va_arg(args, int)), printed = 1;
+		if (format[i] == 'f')
+			printf("%f", va_arg(args, double)), printed = 1;
+		if (format[i] == 's')
 		{
-			/* Second top-level if: handle allowed format characters */
-			if (format[i] == 'c' || format[i] == 'i' ||
-			    format[i] == 'f' || format[i] == 's')
-			{
-				if (format[i] == 'c')
-					printf("%s%c", sep, va_arg(args, int));
-				if (format[i] == 'i')
-					printf("%s%d", sep, va_arg(args, int));
-				if (format[i] == 'f')
-					printf("%s%f", sep, va_arg(args, double));
-				if (format[i] == 's')
-				{
-					str = va_arg(args, char *);
-					if (str == NULL)
-						printf("%s(nil)", sep);
-					else
-						printf("%s%s", sep, str);
-				}
-				sep = ", ";
-			}
-			i++;
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s", str), printed = 1;
 		}
+		i++;
 	}
 
 	va_end(args);
