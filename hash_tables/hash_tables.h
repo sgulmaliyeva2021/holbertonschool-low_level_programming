099113dcf097a41d4bb0c580d99b0cd28f0ce46d
Shola <sholagulmaliyeva@gmail.com>
2026-01-21 09:04:53 +0000

    adding new project's 1st task

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,35 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: key, string
+ * @value: value associated with the key
+ * @next: pointer to the next node in the list
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: size of the array
+ * @array: array of size @size, each element is a pointer to the first node
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
+
