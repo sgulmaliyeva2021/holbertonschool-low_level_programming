5e88dba8c086881d9b0ef0db1b43ab1d5dba584c
Shola <sholagulmaliyeva@gmail.com>
2026-01-21 08:35:15 +0000

    adding 3rd project

--- file_io/main.h
+++ file_io/main.h
@@ -6,9 +6,8 @@
 
 /* Prototype of read_textfile */
 ssize_t read_textfile(const char *filename, size_t letters);
-
 int create_file(const char *filename, char *text_content);
-
+int append_text_to_file(const char *filename, char *text_content);
 
 /* Prototype of _putchar (provided by Holberton) */
 int _putchar(char c);
