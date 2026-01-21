7e7cd1b3357e358ff0ddf842b59fe3fbbc07957e
Shola <sholagulmaliyeva@gmail.com>
2026-01-21 08:48:49 +0000

    editing style less than 40lines

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,22 +3,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int open_from(char *file);
+int open_to(char *file);
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
+void close_fd(int fd);
+
 /**
- * main - copies the content of a file to another file
+ * main - copies content of a file to another file
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success, exits with:
- *         97 if arguments are incorrect
- *         98 if file_from cannot be read
- *         99 if file_to cannot be created or written
- *         100 if a file descriptor cannot be closed
+ * Return: 0 on success, exits with 97,98,99,100 on errors
  */
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t n_read, n_written;
-	char buffer[1024];
 
 	if (ac != 3)
 	{
@@ -26,53 +25,68 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	fd_from = open_from(av[1]);
+	fd_to = open_to(av[2]);
+	copy_file(fd_from, fd_to, av[1], av[2]);
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
+
+/* --- helper functions below --- */
+
+int open_from(char *file)
+{
+	int fd = open(file, O_RDONLY);
+
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 		exit(98);
 	}
+	return (fd);
+}
 
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+int open_to(char *file)
+{
+	int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
+	return (fd);
+}
+
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t n_read, n_written;
+	char buffer[1024];
 
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written != n_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(fd_from);
-			close(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
 
 	if (n_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		close(fd_from);
-		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
+}
 
-	if (close(fd_from) == -1)
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return (0);
 }
